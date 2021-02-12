3d8930ff9fedadd6fd2fc676aaf166d2f2053e66
bartektelec <bartektelec@gmail.com>
2021-02-12 12:06:53 +0100

    fix: :art: add dashes to possible video id params

--- content.js
+++ content.js
@@ -25,7 +25,7 @@ function replaceMessages() {
   document.querySelectorAll('[role="gridcell"]>a').forEach(link => {
     if (link.textContent.includes('youtu')) {
       const prevText = link.textContent;
-      const vidId = prevText.match(/(\w|\d|\_){11}/g);
+      const vidId = prevText.match(/(\w|\d|\_|\-){11}/g);
       link.innerHTML = `<img data-testid="thumbnail" style="width: 100%; margin-top: 1rem" src="https://img.youtube.com/vi/${vidId[0]}/0.jpg" alt="video thumbnail"/><span data-testid="thumbnail">${prevText}</span>`;
     }
   });
